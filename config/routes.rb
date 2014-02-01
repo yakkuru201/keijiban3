da6d2a345fd9f85c7fb7ce08deb0fe2e8c12b417
yakkuru201 <tonamimagic@gmail.com>
2014-02-01 21:05:19 +0900

    generate controller boards

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Keijiban3::Application.routes.draw do
+  get "boards/index"
+  get "boards/new"
+  get "boards/create"
+  get "boards/show"
+  get "boards/edit"
+  get "boards/update"
+  get "boards/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
